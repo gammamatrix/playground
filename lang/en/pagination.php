ea5e09aff038b8effaa95db35beaa5ba2202d26c
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2023-11-20 20:21:37 -0800

    PG-3 - updating table component

---
+++ lang/en/pagination.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pagination Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Used for submitting the form on the table index.
+    |
+    | @see \GammaMatrix\Playground\View\Components\Table\Data
+    */
+
+
+    'go' => 'Go',
+
+    'form.label' => 'Pagination Options',
+
+    'show' => 'Showing page :currentPage of :lastPage with :perPage per page.',
+
+];
